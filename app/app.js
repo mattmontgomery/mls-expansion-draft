29cfeef63dd3ff57f4f727113a0fa782d81319b7
Matt Montgomery <mmontgomery@deseretdigital.com>
2014-11-17 03:38:32 -0700

    Many changes

--- app/app.js
+++ app/app.js
@@ -4,12 +4,18 @@ dotenv.load();
 var Router = require('routes'),
     router = Router();
 
+var static = require('node-static');
+
+var file = new static.Server('./public', { cache: 3600 });
+
 var list = require('./list.js');
 router.addRoute('/list', new list());
 router.addRoute('/list/:team', new list());
 
 var http = require('http');
-var port = (process.env.PORT || 80);
+var port = (process.env.PORT || 80),
+    portStatic = (process.env.PORT_STATIC || 8080);
+
 http.createServer(function (req, res) {
     var match = router.match(req.url);
     if(match) {
@@ -20,4 +26,11 @@ http.createServer(function (req, res) {
     }
 }).listen(port);
 
+http.createServer(function (req, res) {
+    req.addListener('end', function () {
+        file.serve(req, res);
+    }).resume();
+}).listen(portStatic);
+
 console.log('HTTP server listening on port: ' + port );
+console.log('Static HTTP server listening on port: ' + portStatic );
--- app/list.js
+++ app/list.js
@@ -6,20 +6,33 @@ module.exports = function() {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     var redis = require("redis").createClient(rtg.port, rtg.hostname);
     var Cookies = require("cookies");
-    var Promise = require("bluebird");
+    var Promise = require("bluebird"),
+        join = Promise.join;
 
     if(rtg.auth) {
         redis.auth(rtg.auth.split(":")[1]);
     }
 
     var List = function(){};
+    List.prototype.canUpdate = function(ip, team) {
+        return new Promise(function(resolve, reject) {
+            redis.sismember('ips:' + team, ip, function(err, obj) {
+                if(err) {
+                    reject(err);
+                }
+                resolve(false);
+            });
+        });
+    };
     List.prototype.getPlayers = function (data) {
         var players = [];
         if(!_.isObject(data) || !_.isArray(data.list)) {
             return false;
         }
         _.each(data.list,function(val){
-            players[val.toLowerCase()] = 1;
+            if(val) {
+                players[val.toLowerCase()] = 1;
+            }
         });
         return players;
     };
@@ -30,17 +43,48 @@ module.exports = function() {
             redis.hincrby('protected:' + team,player,1);
         });
     };
+    List.prototype.updateTeam = function(team) {
+        redis.hincrby('protected', team, 1);
+    };
+    List.prototype.updateUser = function(ip, team) {
+        redis.sadd('ips:' + team, ip);
+    };
     List.prototype.fetchPlayers = function(team) {
         return new Promise(function(resolve, reject){
             if(!redis.HGETALL('protected:' + team, function(err,obj){
                 if(obj) {
                     resolve(obj);
                 } else {
-                    reject();
+                    reject(err);
                 }
             })) {
                 reject();
             }
+        });
+    };
+    List.prototype.fetchTeam = function(team) {
+        return new Promise(function(resolve, reject) {
+            if(!redis.HGET('protected', team, function(err, obj) {
+                if(obj) {
+                    resolve(obj);
+                } else {
+                    reject(err);
+                }
+            })) {
+                reject();
+            }
+        });
+    };
+    List.prototype.fetchData = function(team) {
+        var self = this;
+        return new Promise(function(resolve, reject) {
+            Promise.all([self.fetchTeam(team), self.fetchPlayers(team)]).done(function(resp){
+                var data = {
+                    votes: resp[0],
+                    players: resp[1]
+                }
+                resolve(data);
+            });
         });
     };
 
@@ -51,6 +95,7 @@ module.exports = function() {
         this.respObj = {};
         this.send = function() {
             response.setHeader("Content-Type", "application/json");
+            response.setHeader("Access-Control-Allow-Origin", "*");
             response.write(JSON.stringify(this.respObj));
             response.end();
         };
@@ -69,57 +114,75 @@ module.exports = function() {
         this.respond_post = function(data) {
             var playersCookie = [],
                 team = 'rsl';
+            var ip = req.connection.remoteAddress;
             var players = _.keys(list.getPlayers(data));
             this.respObj = { status: "failure" };
             if(!data) {
                 response.statusCode = 400;
                 this.respObj.message = "Could not read data";
             }
-            if(players && data) {
-                var failed = false;
-                var reset = data._reset !== undefined ? data._reset : false,
-                    team = (_.isString(data.team) ? data.team : 'RSL').toLowerCase();
-                if(players.length > 11) {
-                    players = false;
-                    list.fetchPlayers(team).done(function(allPlayers) {
-                        response.statusCode = 400;
+            list.canUpdate(ip, team).done(function(hasVoted) {
+                if(players && data && !hasVoted) {
+                    var team = (_.isString(data.team) ? data.team : 'RSL').toLowerCase();
+                    if(players.length > 11) {
+                        players = false;
                         self.respObj = {
                             "status": "failure",
                             "message": "Too many players submitted",
-                            "players": allPlayers
                         };
                         self.send();
-                    });
-                } else if(cookies.get('completed') == 'true' && !reset) {
-                    list.fetchPlayers(team).done(function(allPlayers) {
-                        response.statusCode = 400;
-                        self.respObj = {
-                            "status": "failure",
-                            "message": "Already submitted",
-                            "players": allPlayers
-                        };
-                        self.send();
-                    });
+                    } else if(cookies.get('completed') == 'true') {
+                        list.fetchData(team).done(function(allPlayers) {
+                            response.statusCode = 400;
+                            self.respObj = {
+                                "status": "failure",
+                                "message": "Already submitted",
+                                "players": data.players,
+                                "votes": data.votes
+                            };
+                            self.send();
+                        });
+                    } else if(players.length === 11) {
+                        cookies.set('players', JSON.stringify(players))
+                            .set('submitted', true )
+                            .set('completed', true );
+                        list.updatePlayers(team, players, self.getPlayersFromCookie());
+                        list.updateTeam(team);
+                        list.updateUser(ip,team);
+                        list.fetchData(team).done(function(data) {
+                            response.statusCode = 200;
+                            self.respObj = {
+                                "completed": (players.length === 11),
+                                "status": "success",
+                                "players": data.players,
+                                "votes": data.votes
+                            };
+//                        req.session.completed = true;
+//                        req.session.save();
+                            self.send();
+                        });
+                    } else {
+                        list.fetchData(team).done(function(allPlayers) {
+                            response.statusCode = 400;
+                            self.respObj = {
+                                "status": "incomplete",
+                                "message": "You must protect 11 players",
+                                "players": data.players,
+                                "votes": data.votes
+                            };
+                            self.send();
+                        });
+                    }
+                } else if(hasVoted) {
+                    response.statusCode = 400;
+                    self.respObj.message = 'You can only vote once. Sorry.';
+                    self.send();
+
                 } else {
-                    cookies.set('players', JSON.stringify(players))
-                        .set('submitted', true)
-                        .set('completed', (players.length === 11));
-                    list.updatePlayers(team, players, this.getPlayersFromCookie());
-                    list.fetchPlayers(team).done(function(allPlayers) {
-                        response.statusCode = 200;
-                        self.respObj = {
-                            "completed": (players.length === 11),
-                            "status": "success",
-                            "player_count": players.length,
-                            "players": allPlayers
-                        };
-                        self.send();
-                    });
+                    response.statusCode = 400;
+                    self.send();
                 }
-            } else {
-                response.statusCode = 400;
-                this.send();
-            }
+            });
         };
         this.respond_get = function() {
             var team = match.params.team;
@@ -154,7 +217,6 @@ module.exports = function() {
             });
 
             req.on('end', function() {
-                console.log(req.method);
                 if(req.method === 'POST' || req.method === 'PUT') {
                     self.respond_post(data);
                 } else {
